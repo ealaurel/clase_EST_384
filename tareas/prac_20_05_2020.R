b7629db7fd236d0f0f42dc381c307d84474eedf2
ealaurel <60707980+ealaurel@users.noreply.github.com>
2020-06-07 12:45:21 -0400

    Create prac_20_05_2020.R

---
+++ tareas/prac_20_05_2020.R
@@ -0,0 +1,29 @@
+rm(list = ls())
+X<-matrix(c(rnorm(100,0,1)),20,4)
+X
+kcentro<-function(bd,k,tipo= "media"){
+  nf<-dim(bf)
+  vk<-sample(1:k,dim(bd)[1], replace = T)
+}
+
+sample(1:k,dim(bd)[1], replace = T)
+sample(1:2,dim(X)[1], replace = T)
+
+
+##graficos
+library(ggplot2)
+install.packages("hrbrthemes")
+library(hrbrthemes)
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, shape=Species, alpha=Species, size=Species, color=Species)) + 
+  geom_point() +
+  theme_ipsum()
+
+ggplot(xx, aes(x=x, y=y, color=z)) + 
+  geom_point() +
+  theme_ipsum()
+
+library(readxl)
+read_excel
+
+ufv<-read.table(file = "clipboard", sep = "\t", header=TRUE)
+View(ufv)
